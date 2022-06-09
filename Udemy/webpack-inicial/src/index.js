40940c58de197b1ff88e1fdca7d3983c7361eaeb
RamonPR92 <ramonpr92@hotmail.com>
2022-06-09 12:31:14 -0500

    Configuracion y utilizacion de webpack

---
+++ Udemy/webpack-inicial/src/index.js
@@ -0,0 +1,9 @@
+
+
+//import {funciones a importar, funcion2, funcion3} from 'ruta y nombre del archivo que las contiene'
+import{saludar} from './js/componentes';
+import './style.css';//Para importar un archivo determinado
+
+saludar('Ramon');
+
+// nombre = 'Ramon';
--- hello-world.js
+++
@@ -1,3 +0,0 @@
-
-console.log('Hello world')
-console.log(10+10)
